e402c0d8a097409483aa8e2a59f5a0516e8a98f2
Rushikesh8421 <patilrushikesh8421@gmail.com>
2023-08-08 20:18:21 +0530

    Completed with html and css part moving ahead with js part of the tictactoe game

---
+++ Tic-Tac-Toe/script.js
@@ -0,0 +1,36 @@
+console.log("welcome to tictactoe!")
+
+let music = new Audio("music.mp3")
+let ting = new Audio("ting.mp3")
+let gameOver = new Audio("gameover.mp3")
+
+let turn = "X";
+
+//Function to change the turn
+
+const changeTurn = () =>{
+    return turn === "X" ? "0" : "X";
+}
+
+//Function to check for a win
+
+const checkWin = () =>{
+
+}
+
+//game Logic
+
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(element=>{
+    let boxtext = element.querySelector('.boxtext')
+    element.addEventListener('click',()=>{
+        if(boxtext.innerText === ''){
+            boxtext.innerText = turn;
+            changeTurn();
+            ting.play();
+            checkWin();
+            document.getElementsByClassName("info")[0].innerText = "Turn for "+turn; 
+        }
+    })
+})
+
